cf6839c487da0353b19d69d1238b631dd72fca58
Harshit <rawatharshit300@gmail.com>
2026-07-17 22:52:42 +0530

    frontend changes

--- frontend/src/pages/CaptainHome.jsx
+++ frontend/src/pages/CaptainHome.jsx
@@ -108,13 +108,13 @@ const CaptainHome = () => {
     return (
         <div className='h-screen'>
             <div className='fixed p-6 top-0 flex items-center justify-between w-screen'>
-                <img className='w-16' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+                <img className='w-16' src="https://cdn-icons-png.flaticon.com/512/3081/3081559.png" alt="" />
                 <Link to='/captain-home' className=' h-10 w-10 bg-white flex items-center justify-center rounded-full'>
                     <i className="text-lg font-medium ri-logout-box-r-line"></i>
                 </Link>
             </div>
             <div className='h-3/5'>
-                <img className='h-full w-full object-cover' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="" />
+                <img className='h-full w-full object-cover' src="https://images.unsplash.com/photo-1569336415962-a4bd9f69cd83?q=80&w=2670&auto=format&fit=crop" alt="" />
 
             </div>
             <div className='h-2/5 p-6'>
